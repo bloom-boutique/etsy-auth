80b4110a8c55ad33da3fd74346e5cf555c737f84
ieeemma <emma.freeman@protonmail.com>
2022-08-18 11:35:37 +0100

    Implement auth url generation

--- auth.py
+++ auth.py
@@ -1,17 +1,58 @@
+from __future__ import annotations
 from dataclasses import dataclass
+
+from secrets import token_bytes
+from hashlib import sha256
+from base64 import urlsafe_b64encode
+from requests import Request
+
 from scope import Scope
 
 
-@dataclass
-class EtsyAuth:
+@dataclass(frozen=True)
+class Auth:
     keystring: str
     scopes: list[Scope]
+    redirect: str
 
-    def auth_url(self) -> str:
-        ...
-    
+    def setup_auth(self) -> AuthState:
+        verifier = token_bytes(32)
+        h = sha256()
+        h.update(verifier)
+        sha = h.digest()
+
+        state = token_bytes(32)
+
+        scopes = " ".join([s.value for s in self.scopes])
+
+        r = Request(
+            method="GET",
+            url="https://www.etsy.com/oauth/connect",
+            data={
+                "response_type": "code",
+                "client_id": self.keystring,
+                "redirect_uri": self.redirect,
+                "scope": scopes,
+                "state": urlsafe_b64encode(state),
+                "code_challenge": urlsafe_b64encode(sha),
+                "code_challenge_method": "S256",
+            }
+        )
+        url = r.prepare().url
+        if not url:
+            raise ValueError("Malformed data to URL builder")
+
+        return AuthState(url, verifier, state)
+
     def get_token(self, response: str) -> str:
         ...
-    
+
     def refresh_token(self, token: str) -> str:
         ...
+
+
+@dataclass(frozen=True)
+class AuthState:
+    auth_url: str
+    verifier: bytes
+    state: bytes
--- scope.py
+++ scope.py
@@ -1,54 +1,54 @@
 from enum import Enum
 
 
-class Scope:
+class Scope(Enum):
     ...
 
 
-class Address(Scope, Enum):
+class Address(Scope):
     read = "address_r"
     write = "address_w"
 
 
-class Billing(Scope, Enum):
+class Billing(Scope):
     read = "billing_r"
 
 
-class Cart(Scope, Enum):
+class Cart(Scope):
     read = "cart_r"
     write = "cart_w"
 
 
-class Email(Scope, Enum):
+class Email(Scope):
     read = "email_r"
 
 
-class Favorites(Scope, Enum):
+class Favorites(Scope):
     read = "favorites_r"
     write = "favorites_w"
 
 
-class Listings(Scope, Enum):
+class Listings(Scope):
     delete = "listings_d"
     read = "listings_r"
     write = "listings_w"
 
 
-class Profile(Scope, Enum):
+class Profile(Scope):
     read = "profile_r"
     write = "profile_w"
 
 
-class Recommend(Scope, Enum):
+class Recommend(Scope):
     read = "recommend_r"
     write = "recommend_w"
 
 
-class Shops(Scope, Enum):
+class Shops(Scope):
     read = "shops_r"
     write = "shops_w"
 
 
-class Transactions(Scope, Enum):
+class Transactions(Scope):
     read = "transactions_r"
     write = "transactions_w"
